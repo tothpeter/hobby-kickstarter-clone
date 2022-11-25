7ec3b1d2cec5ede5ec8696049241e53a60a5b767
Peter Toth <tothpeter08@gmail.com>
2022-11-25 15:46:51 -0500

    Style the main header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def bs_icon(name, options = {})
+    final_options = options.deep_dup
+
+    classes = ['bi', "bi-#{name}"]
+    classes.concat(Array(final_options.delete(:class)))
+
+    content_tag(:i, nil, final_options.merge(class: classes))
+  end
 end
