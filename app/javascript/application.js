be7f3b6a8c16cd6a3415f61f9716d4fced1b1d65
Peter Toth <tothpeter08@gmail.com>
2022-11-27 13:52:06 -0500

    Include only the used Bootstrap modules into the final JS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,9 @@
 // Entry point for the build script in your package.json
 import '@hotwired/turbo-rails';
 import './controllers';
-import * as bootstrap from 'bootstrap';
+
+// import * as bootstrap from 'bootstrap';
+// import 'bootstrap/js/src/modal'
 
 import { Turbo } from '@hotwired/turbo-rails';
 Turbo.session.drive = false;
